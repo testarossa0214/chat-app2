6a2b6915167556eba7c55dda865d8af7493d3608
testarossa0214 <85383842+testarossa0214@users.noreply.github.com>
2021-06-18 19:27:54 +0900

    各モデルのテスト

---
+++ spec/factories/messages.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :message do
+    content {Faker::Lorem.sentence}
+    association :user
+    association :room
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
+  end
+end
